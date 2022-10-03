fe3769f2ad4aadbee038b586762398883219a8b6
hafidkrntn <hafidkrntn@gmail.com>
2022-10-03 22:07:10 +0700

    part 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,13 +42,17 @@ const Home = ({
 
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals}/>
         <section className="md:space-y-24">
         <Row title="Trending Now" movies={trendingNow} />
